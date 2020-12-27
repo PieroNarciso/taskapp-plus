3a6d7735fcfc64ebc9b5a574dd90675690556a1b
Piero Narciso <pieronarciso988@gmail.com>
2020-12-27 13:12:10 -0500

    Added authorization functionality

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
 import * as controller from '../controllers/task.controller';
+import { authenticateJWT } from '../auth';
 
 
 const router = Router();
 
-router.get('/', controller.getAllByUserId);
-router.post('/', controller.postCreateTask);
-router.put('/:_id', controller.updateTaskById);
-router.delete('/:_id', controller.deleteTaskById);
+router.get('/', authenticateJWT, controller.getAll);
+router.post('/', authenticateJWT, controller.postCreateTask);
+router.put('/:_id', authenticateJWT, controller.updateTaskById);
+router.delete('/:_id', authenticateJWT, controller.deleteTaskById);
 
 export default router;
